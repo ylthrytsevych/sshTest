65623ea5a9e4b0e3f5fa23e02c55a03173e2e0e6
hrytsevychyt <hrytsevychyt@dtek.com>
2025-12-16 16:09:06 +0200

    Initial commit

---
+++ Converter/src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+class Main {
+
+    public static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter app");
+
+        double kgs = 5;
+//        double pounds = convKgsToPounds(kgs);
+//        System.out.printf("Result is %.2f pounds.%n", pounds);
+    }
+
+//    private static double convKgsToPounds(double kgs) {
+//        return kgs * CONV_K;
+//    }
+}
